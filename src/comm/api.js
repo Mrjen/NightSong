1a388a7f60a3e346ba1f37a1e142df42cefba9f8
SC-201704272314\Administrator <18372668568@163.com>
2017-11-03 14:28:13 +0800

    首版提交

---
+++ src/comm/api.js
@@ -0,0 +1,8 @@
+// 主域名
+const apiUrl = "https://utest.playonwechat.com"
+// 首页数据
+const Home = apiUrl + '/music/App/home'
+
+module.exports  = {
+    Home
+}
---
+++ src/comm/wxRequest.js
@@ -0,0 +1,28 @@
+wx.myRequest = function(data = {},url,cb) {
+    const cookie = wx.getStorageSync('cookie');
+    let extConfig = wx.getExtConfigSync ? wx.getExtConfigSync() : {}
+    console.log("第三方平台数据默认329",extConfig);
+    data.kid = extConfig.kid?extConfig.kid:'329';
+    const requestOption = {
+        url: url,
+        data: data,
+        method:data.method?data.method:"get",
+        header: {
+            Cookie: ''
+        },
+        success: function(res) {
+            if (res.header['Set-Cookie']) {
+                var regexp = /(PHPSESSID=.*);/;
+                var cookie = res.header['Set-Cookie'].match(regexp)[1];
+                wx.setStorageSync('cookie', cookie)
+            }
+            typeof cb == 'function' && cb(res);
+        }
+    };
+    if (cookie) {
+        requestOption.header.Cookie = cookie;
+    } else {
+        delete requestOption.header.Cookie;
+    }
+    this.request(requestOption);
+};
